1669f0d9e7235bae912a33c683050f50e69021f4
Carlos Tolentino <carlos.tolentinoe@gmail.com>
2019-01-29 22:59:07 -0400

    Filtrando los proyectos

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -12,12 +12,26 @@ import * as $ from 'jquery';
 export class PortfolioComponent implements OnInit {
 
   public data;
+  public filterData;
+  public selected:String
 
   constructor(private _dataServices: DataInformationService) { }
 
   ngOnInit() {
     this.data = JSON.parse(localStorage.getItem('portfoliopage'));    
-    console.log(this.data.projects);
+    this.filterData = this.data.projects;
+    this.selected = 'ALL'
+  }
+
+  onChangeFilter(filter){
+    if(filter == 'ALL'){
+      this.filterData = this.data.projects;
+    }else{
+      this.filterData = this.data.projects.filter(word => word.category == filter)
+    }
+
+    this.selected = filter;
+    
   }
   
 
