44c421c5efeb95e6d00c540c477713f4bab9a997
Carlos Tolentino <carlos.tolentinoe@gmail.com>
2019-02-23 20:37:33 -0400

    Completada

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDGUPLgS6zHSpk05yvujTIhEpKH1qjh0j0",
+    authDomain: "cv-angular-landing.firebaseapp.com",
+    databaseURL: "https://cv-angular-landing.firebaseio.com",
+    projectId: "cv-angular-landing",
+    storageBucket: "cv-angular-landing.appspot.com",
+    messagingSenderId: "113644221776"
+  }
 };
